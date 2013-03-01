a5f6af2e75faeaaef79cefc77c40be0dee2c4872
David Wettstein <dwettstein@gmail.com>
2013-03-01 17:30:30 +0100

    Added a batch to run trace.

---
+++ run_trace.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd D:\David\Eclipse Projects\PropertiesAnalyzer\bin
+
+prompt $g
+
+Java -cp "D:\David\Eclipse Projects\PropertiesAnalyzer\tools.jar;D:\David\Eclipse Projects\PropertiesAnalyzer\bin" Trace Count
+
+echo.
+
+cmd /k
+
