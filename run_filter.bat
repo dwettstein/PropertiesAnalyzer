723789a40389703de78671c43889caf65959a243
David Wettstein <dwettstein@gmail.com>
2013-03-07 08:14:25 +0100

    Re-added batch file.

---
+++ run_filter.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd D:\David\Eclipse Projects\PropertiesAnalyzer\bin
+
+prompt $g
+
+Java -cp "D:\David\Eclipse Projects\PropertiesAnalyzer\tools.jar;D:\David\Eclipse Projects\PropertiesAnalyzer\bin" Filter Count
+
+echo.
+
+cmd /k
+
